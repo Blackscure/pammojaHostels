21adff5f380edd5ce3362135882e4c9163660a94
Blackscure <Wekesabuyahi@gmail.com>
2020-03-04 10:37:04 +0300

    validating phone no, email, and message lengh

--- Js/Script.js
+++ Js/Script.js
@@ -23,3 +23,19 @@ var text;
     error_message.innerHTML = text;
     return false;
   }
+
+  if(isNaN(phone) || phone.length != 10){
+    text = "Please Enter valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(email.indexOf("@") == -1 || email.length < 6){
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(message.length <= 140){
+    text = "Please Enter More Than 140 Characters";
+    error_message.innerHTML = text;
+    return false;
+  }
