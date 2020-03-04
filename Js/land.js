da5dcc83606e94f056458ed896292853f08035f7
kevin-kili <kilikevin435@gmail.com>
2020-03-04 10:42:26 +0300

    added attempts in login

--- Js/land.js
+++ Js/land.js
@@ -12,7 +12,30 @@ function passValidator(){
     if(password1===password2){
         alert("Success");
     }else if(password1 != password2){
-        alert("Passwords are not the same!");
+        // alert("Passwords are not the same!");
         break;
     }
+}
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+if ( email == "" && password == "password1||password2"){
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("email").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("login").disabled = true;
+return false;
+}
+}
 }
