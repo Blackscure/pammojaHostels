e27eb9e5c421163e259295ea7e6775b4eab57839
Blackscure <Wekesabuyahi@gmail.com>
2020-03-04 08:05:12 +0300

    function added and eeror mssge assigned to 10px

---
+++ Js/Script.js
@@ -0,0 +1,12 @@
+function validation(){
+      var name = document.getElementById("name").value;
+      var subject = document.getElementById("subject").value;
+      var phone = document.getElementById("phone").value;
+      var email = document.getElementById("email").value;
+      var error_message = document.getElementById("error_meassage").value;
+      var text;
+
+      error_message.style.padding = "10px";
+
+      
+}
