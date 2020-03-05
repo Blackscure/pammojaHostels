976aca28eeee95b5421024c63fc4f19e7367f5aa
SaintieMonte10 <claremonte53@gmai.com>
2020-03-05 21:44:53 +0300

    added functionality

---
+++ Js/clare.js
@@ -0,0 +1,225 @@
+$(document).ready(function () {
+  $("#profile1").hover(function () {
+    $(".img1").toggle()
+    $(".text1").toggle()
+  })
+})
+
+$(document).ready(function () {
+  $("#profile2").hover(function () {
+    $(".img2").toggle()
+    $(".text2").toggle()
+  })
+})
+
+$(document).ready(function () {
+  $("#profile3").hover(function () {
+    $(".img3").toggle()
+    $(".text3").toggle()
+  })
+})
+
+$(document).ready(function () {
+  $("#profile4").hover(function () {
+    $(".img4").toggle()
+    $(".text4").toggle()
+  })
+})
+
+$(document).ready(function () {
+  $("#profile5").hover(function () {
+    $(".img5").toggle()
+    $(".text5").toggle()
+  })
+})
+
+$(document).ready(function () {
+  $("#profile6").hover(function () {
+    $(".img6").toggle()
+    $(".text6").toggle()
+  })
+})
+
+$(document).ready(function () {
+  $("#profile7").hover(function () {
+    $(".img7").toggle()
+    $(".text7").toggle()
+  })
+})
+
+$(document).ready(function () {
+  $("#profile8").hover(function () {
+    $(".img8").toggle()
+    $(".text8").toggle()
+  })
+})
+
+$(document).ready(function () {
+  $("#profile9").hover(function () {
+    $(".img9").toggle()
+    $(".text9").toggle()
+  })
+})
+
+
+
+function wendy() {
+  Swal.fire({
+    title: 'Are you sure you want Wendie as your roomie?',
+    text: "You won't be able to revert this!",
+    icon: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, I am sure!'
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire(
+        'Saved!',
+        'Your have booked Wendie as your roomie.',
+        'success'
+      )
+    }
+  })
+}
+
+function brandon() {
+  Swal.fire({
+    title: 'Are you sure you want Brandon as your roomie?',
+    text: "You won't be able to revert this!",
+    icon: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, I am sure!'
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire(
+        'Saved!',
+        'Your have booked Brandon as your roomie.',
+        'success'
+      )
+    }
+  })
+}
+
+function cindy() {
+  Swal.fire({
+    title: 'Are you sure you want Cindy as your roomie?',
+    text: "You won't be able to revert this!",
+    icon: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, I am sure!'
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire(
+        'Saved!',
+        'Your have booked Cindy as your roomie.',
+        'success'
+      )
+    }
+  })
+}
+
+function farahan() {
+  Swal.fire({
+    title: 'Are you sure you want Farahan our roomie?',
+    text: "You won't be able to revert this!",
+    icon: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, I am sure!'
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire(
+        'Saved!',
+        'Your have booked Farahan as your roomie.',
+        'success'
+      )
+    }
+  })
+}
+
+function seven() {
+  Swal.fire({
+    title: 'Are you sure you want Seven as your roomie?',
+    text: "You won't be able to revert this!",
+    icon: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, I am sure!'
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire(
+        'Saved!',
+        'Your have booked Seven as your roomie.',
+        'success'
+      )
+    }
+  })
+}
+
+
+function margarita() {
+  Swal.fire({
+    title: 'Are you sure you want joram as your roomie?',
+    text: "You won't be able to revert this!",
+    icon: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, I am sure!'
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire(
+        'Saved!',
+        'Your have booked Joram as your roomie.',
+        'success'
+      )
+    }
+  })
+}
+
+function john() {
+  Swal.fire({
+    title: 'Are you sure you want John as your roomie?',
+    text: "You won't be able to revert this!",
+    icon: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, I am sure!'
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire(
+        'Saved!',
+        'Your have booked Margarita as your roomie.',
+        'success'
+      )
+    }
+  })
+}
+
+function montana() {
+  Swal.fire({
+    title: 'Are you sure you want Montana as your roomie?',
+    text: "You won't be able to revert this!",
+    icon: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, I am sure!'
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire(
+        'Saved!',
+        'Your have booked Joram as your roomie.',
+        'success'
+      )
+    }
+  })
+}
